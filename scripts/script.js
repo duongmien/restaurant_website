f1be9cbf7695b3dc35bafd534756e104d89e0bca
duongmien <duongthimien7@gmail.com>
2021-08-21 23:05:29 +0700

    update section home

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,15 @@ document.addEventListener("click",function(e){
     if(e.target.closest(".nav-item")){
         toggleNav();
     }
-})
+});
+
+// Sticky header
+window.addEventListener("scroll",function(){
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky")
+    }
+    else{
+        document.querySelector(".header").classList.remove("sticky")
+    }
+
+});
