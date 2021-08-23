6ae25cf76c9ede07f2d94808c4c3e20d7812e305
duongmien <duongthimien7@gmail.com>
2021-08-23 18:03:32 +0700

    update menu section

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,16 @@ window.addEventListener("scroll",function(){
     }
 
 });
+
+// Menu tabs
+const menuTabs =document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target= e.target.getAttribute("data-target")
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section")
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active")
+        menuSection.querySelector(target).classList.add("active");
+    }
+});
